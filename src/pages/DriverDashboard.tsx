dd1bd0cb176719d61d894edd6c516ea23d985f65
Tauhid hossen <tauhidhossen04@gmail.com>
2025-10-29 16:01:57 +0600

    send to the rider request to the driver succecessfull test by post man token

--- src/pages/DriverDashboard.tsx
+++ src/pages/DriverDashboard.tsx
@@ -1,11 +1,21 @@
-import React from "react";
-import DriverIncomingRequests from "../components/DriverIncomingRequests";
+
+import DriverIncomingRequests from '../components/DriverIncomingRequests'
+import SummaryCards from '../components/SummaryCards'
 
 export default function DriverDashboard() {
   return (
-    <div className="min-h-screen bg-gray-50 p-6">
-      <h1 className="text-2xl font-bold mb-4">Driver Dashboard</h1>
-      <DriverIncomingRequests />
+    <div className="p-4">
+      <SummaryCards />
+      <div className="mt-6 grid grid-cols-1 lg:grid-cols-3 gap-6">
+        <div className="col-span-1 lg:col-span-2">
+          {/* main driver workspace */}
+          <h2 className="text-lg font-semibold mb-3">Active Area</h2>
+          {/* other content */}
+        </div>
+        <div className="col-span-1">
+          <DriverIncomingRequests />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
